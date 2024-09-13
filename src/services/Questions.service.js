51ffcc504a84fac72681244274b0642192eb5bb3
EricWithC04 <negativegame117@gmail.com>
2024-09-13 14:53:30 -0300

    feat: Evaluation service

--- src/services/Questions.service.js
+++ src/services/Questions.service.js
@@ -1,11 +1,11 @@
 import QuestionModel from "../models/Questions.model";
 
-export const findAllQuestions = async () => {
+export const getAllQuestions = async () => {
     const questions = await QuestionModel.findAll();
     return questions;
 }
 
-export const findOneQuestion = async (id) => {
+export const getOneQuestion = async (id) => {
     const question = await QuestionModel.findByPk(id);
     return question;
 }
